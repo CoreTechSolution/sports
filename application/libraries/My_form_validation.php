05eb53b30b2ed8cbbf1d07919ae3d38e2a699198
Coregen Solutions <30926757+CoreTechSolution@users.noreply.github.com>
2018-10-26 17:28:10 +0530

    Delete My_form_validation.php

--- application/libraries/My_form_validation.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-class My_form_validation extends CI_Form_validation
-{
-	function run($module = '', $group = '') {
-		(is_object($module)) AND $this->CI =& $module;
-		return parent::run($group);
-	}
-}
